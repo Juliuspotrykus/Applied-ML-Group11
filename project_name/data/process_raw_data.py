c7ac27bc314e2fed25f863e62b280af979941448
lu56ad07 <lu56ad07@isdedu.de>
2026-05-10 19:14:21 +0200

    feat: added code to locate bombay and jakarta files to remove

--- project_name/data/process_raw_data.py
+++ project_name/data/process_raw_data.py
@@ -17,3 +17,18 @@ image_rgb, label_rgb = rgb_dataset[0]
 # shape of images is 3, 64, 64
 print(image_rgb.shape)    
 print(label_rgb)          
+
+# 13 band SeaLake folder contains 597 incorrect files that need to be removed
+ms_path = os.path.join(path, "EuroSATallBands")
+sealake_path_ms = os.path.join(ms_path, "SeaLake")
+
+bombay_files = [f for f in os.listdir(sealake_path_ms) if f.startswith("Bombay")]
+jakarta_files = [f for f in os.listdir(sealake_path_ms) if f.startswith("Jakarta")]
+
+print("Bombay files:", len(bombay_files))
+print("Jakarta files:", len(jakarta_files))
+print("Total to remove:", len(bombay_files) + len(jakarta_files))
+
+# TODO: uncomment code to delete files
+# for f in bombay_files + jakarta_files:
+#     os.remove(os.path.join(sealake_path_ms, f))
