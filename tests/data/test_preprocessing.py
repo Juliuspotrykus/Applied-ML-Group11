03f36fca9ee2571295bcf9023989517203751219
tiemeboerema <tiemeboerema@gmail.com>
2026-05-18 13:07:05 +0200

    test: create test for MS preprocessing; possibly add more extensive tests later

--- tests/data/test_preprocessing.py
+++ tests/data/test_preprocessing.py
@@ -1,15 +1,35 @@
 import unittest
-import torch
 
-from project_name.data.preprocessors import normalize_RGB_img
+import torch
+from project_name.data.preprocessors import normalize_MS_img, normalize_RGB_img
 
 
 class PreprocessingTest(unittest.TestCase):
     def test_RGB(self):
-        # Generate a random tensor with shape (3, 64, 64)
+        # Generate a random tensor with shape (3, 64, 64).
         rgb_img = torch.randn(3, 64, 64)
         preprocessed_img = normalize_RGB_img(rgb_img)
         self.assertTrue(torch.equal(preprocessed_img, rgb_img / 255))
+
+    def test_MS(self):
+        # Generate a random tensor with shape (13, 64, 64),
+        # filled with somewhat realistic values.
+        ms_img = torch.randint(
+            low=0,
+            high=10000,
+            size=(13, 64, 64),
+        )
+
+        preprocessed_img = normalize_MS_img(ms_img)
+
+        # For each band, check that the values are between the
+        # expected minimum and expected maximum values.
+        # TODO
+
+        # Sanity check that all values are between -3 and 3,
+        # should be consistent because of the clipping.
+        self.assertTrue(torch.all(preprocessed_img >= -3))
+        self.assertTrue(torch.all(preprocessed_img <= 3))
 
 
 if __name__ == "__main__":
