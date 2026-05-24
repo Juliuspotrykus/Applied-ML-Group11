f3ed0665162dbd7415b3323bee145a2a18ea6fbb
lu56ad07 <lu56ad07@isdedu.de>
2026-05-24 16:51:33 +0200

    feat: gradcam for rgb images implemented

--- eurosat_classification/features/gradcam.py
+++ eurosat_classification/features/gradcam.py
@@ -1,18 +1,42 @@
 from .retrieve_model import get_model
 from ..models.cnn import CNN
 import torch.nn as nn
+from torch import Tensor
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.image import show_cam_on_image
+import numpy as np
 
 
 MODEL_PATH = "models/model1.pkl"
 
 model = get_model(MODEL_PATH)
 
-def get_last_conv_layer(model: CNN):
+def get_last_conv_layer(model: CNN) -> list[nn.Conv2d]:
     last_layer = None
     
     for layer in model.backbone:
         if isinstance(layer, nn.Conv2d):
             last_layer = layer
 
-    return last_layer
+    if last_layer is None:
+        raise ValueError("No last layer found")
+
+    return [last_layer]
+
+def visualize_gradcam_rgb(model: CNN, input_tensor: Tensor, input_rgb_image: np.ndarray):
+    """
+    Args:
+        model (CNN): CNN model used for prediction
+        input_tensor (Tensor): (1, 3, 64, 64) shaped tensor of input image
+        input_rgb_image (np.ndarray): (64, 64, 3) array of input image, normalized to [0, 1]
+    """
+    # Note: We can also make the target layer be all the layers, in which case they are aggregated
+    with GradCAM(model=model, target_layers=get_last_conv_layer(model)) as cam:
+        # targets as none defaults to highest scoring category (per batch)
+        grayscale_cam = cam(input_tensor=input_tensor,targets=None)
+        
+        # In this example grayscale_cam has only one image in the batch:
+        grayscale_cam = grayscale_cam[0, :]
+        
+        return show_cam_on_image(input_rgb_image, grayscale_cam, use_rgb=True)
 
