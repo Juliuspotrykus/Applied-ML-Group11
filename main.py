72817827bf0e2d78808b01ac414b2e38c506195e
tiemeboerema <tiemeboerema@gmail.com>
2026-05-25 23:45:53 +0200

    feat: create first outline of API; delete redundant test_main

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
-def hello_world():
-    return "Hello, World!"
+app = FastAPI(
+    title="Satellite Image Classifier",
+    summary="""An API endpoint to classify satellite images into one of ten
+            classes using a CNN. Trained using the EuroSAT dataset.""",
+    description="""
+# An API endpoint to access CNN classifiers trained on the EuroSAT dataset.
+Two models are accessible; one trained on RGB images (3-dimensional),
+and one trained on multispectral images (13-dimensional).
+
+## Model Usage - RGB
+This model is trained on 64x64 RGB images of Sentinel-2 satellite data.
+
+## Model Usage - MS
+This model is trained on 64x64 multispectral images of Sentinel-2 satellite
+data.
+
+    """,
+    version="alpha",
+)
 
 
-if __name__ == '__main__':
-    hello_world()
+@app.get("/", description="Root endpoint that redirects to documentation.")
+async def root():
+    return RedirectResponse(url="/docs")
