e744f0355c8cdb7afa09ea541600b07781344696
tiemeboerema <tiemeboerema@gmail.com>
2026-05-25 23:16:27 +0200

    refactor: create setUp in preprocessing test

--- tests/data/test_preprocessing.py
+++ tests/data/test_preprocessing.py
@@ -5,25 +5,25 @@ from eurosat_classification.data.preprocessors import normalize_MS_img
 
 
 class PreprocessingTest(unittest.TestCase):
-    def test_MS(self):
+    def setUp(self):
         # Generate a random tensor with shape (13, 64, 64),
         # filled with somewhat realistic values.
-        ms_img = torch.randint(
+        self.ms_img = torch.randint(
             low=0,
             high=10000,
             size=(13, 64, 64),
         )
+        self.preprocessed_ms_img = normalize_MS_img(self.ms_img)
 
-        preprocessed_img = normalize_MS_img(ms_img)
-
+    def test_MS(self):
         # For each band, check that the values are between the
         # expected minimum and expected maximum values.
         # TODO
 
         # Sanity check that all values are between -3 and 3,
         # should be consistent because of the clipping.
-        self.assertTrue(torch.all(preprocessed_img >= -3))
-        self.assertTrue(torch.all(preprocessed_img <= 3))
+        self.assertTrue(torch.all(self.preprocessed_ms_img >= -3))
+        self.assertTrue(torch.all(self.preprocessed_ms_img <= 3))
 
 
 if __name__ == "__main__":
