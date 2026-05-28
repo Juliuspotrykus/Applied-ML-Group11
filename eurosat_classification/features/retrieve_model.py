711615689dd6497a5881d74e6670ff7fe11cef12
lu56ad07 <lu56ad07@isdedu.de>
2026-05-28 15:55:58 +0200

    fix: adjust get model function to load to cpu

--- eurosat_classification/features/retrieve_model.py
+++ eurosat_classification/features/retrieve_model.py
@@ -2,4 +2,4 @@ import torch
 from ..models.cnn import CNN
 
 def get_model(pkl_path: str) -> CNN:
-    return torch.load(pkl_path)
+    return torch.load(pkl_path, map_location="cpu", weights_only=False)
